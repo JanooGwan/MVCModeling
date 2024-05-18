5308cea35dde1db4641e52431159c125fc8bf942
JanooGwan <janoogwan39@gmail.com>
2024-05-19 00:03:06 +0900

    SpringMVCStudy Repository 연결

---
+++ src/main/java/com/example/TestMVC/UserController.java
@@ -0,0 +1,53 @@
+package com.example.TestMVC;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @PostMapping("")
+    public UserDTO insertUser(@RequestBody UserDTO user) {
+        return userService.insertUser(user);
+    }
+
+    @ResponseBody
+    @GetMapping("/members")
+    public String members() { return "members"; }
+
+    @GetMapping("")
+    public List<UserDTO> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{userName}")
+    public UserDTO getUserByUserName(@PathVariable String userName) {
+        return userService.getUserByUserName(userName);
+    }
+
+    @PutMapping("/{userName}")
+    public void updateUserName(@PathVariable String userName, @RequestBody UserDTO user) {
+        userService.updateUserName(userName, user);
+    }
+
+    @PutMapping("/{userInfo}")
+    public void updateUserInfo(String userName, UserDTO user) {
+        userService.updateUserInfo(userName, user);
+    }
+
+    @DeleteMapping("/{userName}")
+    public void deleteUser(@PathVariable String userName) {
+        userService.deleteUser(userName);
+    }
+}
+
+
+
+//
---
+++ src/test/java/com/example/TestMVC/TestMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
