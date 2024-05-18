d429fcb0f792be5546d36e4ad17ba2d243e80bea
JanooGwan <janoogwan39@gmail.com>
2024-05-19 08:34:01 +0900

    html 작성 완료 및 html로 데이터 출력 기능 구현

--- src/main/java/com/example/TestMVC/PageController.java
+++ src/main/java/com/example/TestMVC/PageController.java
@@ -1,14 +1,19 @@
 package com.example.TestMVC;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PageController {
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/members")
-    public String members(Model model) {
-        model.addAttribute("users", UserRepository.users);
+    public String getAllUsers(Model model) {
+        model.addAttribute("users", userRepository.getAllUsers());
         return "members";
     }
 }
